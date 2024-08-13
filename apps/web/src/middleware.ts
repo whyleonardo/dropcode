2b8d2970701344ab8fb1e4e9fbca71092c519e49
Christian Leonardo <pleasefloyd73@gmail.com>
2024-08-13 19:00:18 -0300

    feat: setup basic route protection

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,1 +1,17 @@
-export { auth as middleware } from "@soli/auth"
+import { auth as middleware } from "@soli/auth"
+
+export default middleware((req) => {
+  if (!req.auth && req.nextUrl.pathname !== "/auth/login") {
+    const newUrl = new URL("/login", req.nextUrl.origin)
+    return Response.redirect(newUrl)
+  }
+
+  if (req.auth && req.nextUrl.pathname === "/auth/login") {
+    const newUrl = new URL("/workspace", req.nextUrl.origin)
+    return Response.redirect(newUrl)
+  }
+})
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
