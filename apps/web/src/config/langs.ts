816a2758effdc4f873dce04823c8c16d7fe2f88b
Christian Leonardo <pleasefloyd73@gmail.com>
2024-08-21 14:09:21 -0300

    chore: adjust langs config and sync with db enums

--- apps/web/src/config/langs.ts
+++ apps/web/src/config/langs.ts
@@ -1,39 +1,47 @@
 import { Icons } from "@/components/icons"
 
-export const langs = {
+import type { LucideProps } from "lucide-react"
+import type { BundledLanguage } from "shiki/bundle/web"
+
+type Lang = Record<
+  string,
+  {
+    id: BundledLanguage
+    name: string
+    icon: (props: LucideProps) => JSX.Element
+    extension: string
+  }
+>
+
+export const langs: Lang = {
   ANGULAR: {
     id: "angular-ts",
     name: "Angular",
     icon: Icons.angular,
     extension: "angular",
   },
-  // ASTRO: { id: "astro", name: "Astro", icon: Icons.astro, extension: "" },
+  ASTRO: { id: "astro", name: "Astro", icon: Icons.astro, extension: "astro" },
   CSS: { id: "css", name: "CSS", icon: Icons.css, extension: "css" },
-  // HTML: { id: "html", name: "HTML", icon: Icons.html, extension: "" },
-  // JAVA: { id: "java", name: "Java", icon: Icons.java, extension: "" },
+  CPP: { id: "cpp", name: "C++", icon: Icons.cpp, extension: "cpp" },
+  HTML: { id: "html", name: "HTML", icon: Icons.html, extension: "html" },
+  JAVA: { id: "java", name: "Java", icon: Icons.java, extension: "java" },
   JAVASCRIPT: {
     id: "javascript",
     name: "JavaScript",
     icon: Icons.javascript,
     extension: "js",
   },
-  JSON: { id: "json", name: "JSON", icon: Icons.json, extension: "json" },
-  // JSX: { id: "jsx", name: "JSX", icon: Icons.jsx, extension: "" },
-  // MARKDOWN: { id: "markdown", name: "Markdown", icon: Icons.markdown, extension: "" },
-  // MDX: { id: "mdx", name: "MDX", icon: Icons.mdx, extension: "" },
-  // PHP: { id: "php", name: "PHP", icon: Icons.php, extension: "" },
-  // POSTCSS: { id: "postcss", name: "PostCSS", icon: Icons.postcss, extension: "" },
-  // PYTHON: { id: "python", name: "Python", icon: Icons.python, extension: "" },
-  // RUBY: { id: "ruby", name: "Ruby", icon: Icons.ruby, extension: "" },
-  // SASS: { id: "sass", name: "Sass", icon: Icons.sass, extension: "" },
-  // SCSS: { id: "scss", name: "SCSS", icon: Icons.scss, extension: "" },
+  JSON: { id: "jsonc", name: "JSON", icon: Icons.json, extension: "json" },
+  JSX: { id: "jsx", name: "JSX", icon: Icons.jsx, extension: "jsx" },
+  PHP: { id: "php", name: "PHP", icon: Icons.php, extension: "php" },
+  PYTHON: { id: "python", name: "Python", icon: Icons.python, extension: "py" },
   SVELTE: {
     id: "svelte",
     name: "Svelte",
     icon: Icons.svelte,
     extension: "svelte",
   },
-  // TSX: { id: "tsx", name: "TSX", icon: Icons.tsx, extension: "" },
+  TSX: { id: "tsx", name: "TSX", icon: Icons.tsx, extension: "tsx" },
   TYPESCRIPT: {
     id: "typescript",
     name: "TypeScript",
@@ -41,5 +49,5 @@ export const langs = {
     extension: "ts",
   },
   VUE: { id: "vue", name: "Vue", icon: Icons.vue, extension: "vue" },
-  // YAML: { id: "yaml", name: "YAML", icon: Icons.yaml, extension: "" },
+  YAML: { id: "yaml", name: "YAML", icon: Icons.yaml, extension: "yaml" },
 } as const
