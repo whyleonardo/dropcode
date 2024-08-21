3793b8b6ac50701fa20e7a830d9e1df14e4d558b
Christian Leonardo <pleasefloyd73@gmail.com>
2024-08-21 11:42:08 -0300

    refactor: add collection name when on collection page

--- apps/web/src/app/(app)/workspace/collections/_components/back-page-button.tsx
+++ apps/web/src/app/(app)/workspace/collections/_components/back-page-button.tsx
@@ -2,19 +2,24 @@
 
 import { usePathname, useRouter } from "next/navigation"
 
-import { ArrowLeft } from "lucide-react"
+import { ArrowLeft, Folder } from "lucide-react"
 
 export const BackPageButton = () => {
   const router = useRouter()
+  const pathname = usePathname()
+
+  const isSnippetPathname = pathname.split("/").at(3)
 
   return (
     <button
       type="button"
       onClick={() => router.back()}
-      className="group flex items-center gap-1"
+      className="group flex items-center gap-1.5"
     >
       <ArrowLeft className="text-gray-10 group-hover size-4 transition-all group-hover:-translate-x-0.5" />
-      <h2 className="font-mono text-lg font-medium">Collections</h2>
+      <h2 className="font-mono text-lg font-medium">
+        {isSnippetPathname ?? "Collections"}
+      </h2>
     </button>
   )
 }
