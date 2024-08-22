31b448393ca233819b5bd90867de5e49fd6454d7
Christian Leonardo <pleasefloyd73@gmail.com>
2024-08-22 16:14:44 -0300

    feat: create util function to handle slugs creation

---
+++ apps/web/src/utils/create-slug.ts
@@ -0,0 +1,9 @@
+export const createSlug = (text: string): string => {
+  return text
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^\w\s]/gi, "")
+    .trim()
+    .replace(/\s+/g, "-")
+    .toLowerCase()
+}
