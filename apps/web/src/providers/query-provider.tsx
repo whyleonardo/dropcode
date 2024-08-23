ba49c30d3bc4ff1e72c5ca78903cc8480b90df8d
Christian Leonardo <pleasefloyd73@gmail.com>
2024-08-23 10:11:33 -0300

    refactor: init queryClient with a state and define stale time of queries

--- apps/web/src/providers/query-provider.tsx
+++ apps/web/src/providers/query-provider.tsx
@@ -1,13 +1,23 @@
 "use client"
 
+import { useState } from "react"
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
-const queryClient = new QueryClient()
-
 export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 60 * 1000,
+        },
+      },
+    })
+  )
+
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={client}>
       {children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
