12b0a8f8ef4426d34e5c2fca719f88239b35c9ff
Christian Leonardo <pleasefloyd73@gmail.com>
2024-08-23 16:05:46 -0300

    refactor: not remove hyphens when create slug from text

--- apps/web/src/utils/create-slug.ts
+++ apps/web/src/utils/create-slug.ts
@@ -2,7 +2,7 @@ export const createSlug = (text: string): string => {
   return text
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "")
-    .replace(/[^\w\s]/gi, "")
+    .replace(/[^\w\s-]/gi, "")
     .trim()
     .replace(/\s+/g, "-")
     .toLowerCase()
