2f8ef139bba14341add2b6def2de5ec7a6b89ab9
Christian Leonardo <pleasefloyd73@gmail.com>
2024-08-26 11:06:36 -0300

    feat: displays modal confirmation on file delete

--- apps/web/src/app/(workspace)/snippet/[snippetSlug]/_components/tab-trigger.tsx
+++ apps/web/src/app/(workspace)/snippet/[snippetSlug]/_components/tab-trigger.tsx
@@ -3,12 +3,22 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { useRef } from "react"
 
+import { Button, buttonVariants } from "@/components/ui/button"
 import {
   ContextMenu,
   ContextMenuContent,
   ContextMenuItem,
   ContextMenuTrigger,
 } from "@/components/ui/context-menu"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import { TabsTrigger } from "@/components/ui/tabs"
 
 import { langs } from "@/config/langs"
@@ -43,45 +53,75 @@ export const TabTrigger = ({ file }: { file: File }) => {
   const pathname = usePathname()
 
   return (
-    <ContextMenu>
-      <ContextMenuTrigger>
-        <TabsTrigger
-          key={file.id}
-          value={file.id}
-          ref={triggerRef}
-          className="space-x-2 font-mono"
-          onClick={() => router.push(`${pathname}?tabFileId=${file.id}`)}
-        >
-          <Icon className="size-3.5" />
-          <span>
-            {file.name}.{langs[file.language].extension}
-          </span>
-        </TabsTrigger>
-      </ContextMenuTrigger>
-      <ContextMenuContent>
-        <ContextMenuItem className="cursor-pointer">
-          <Pencil className="mr-2 size-4" />
-          Edit
-        </ContextMenuItem>
+    <Dialog>
+      <ContextMenu>
+        <ContextMenuTrigger>
+          <TabsTrigger
+            key={file.id}
+            value={file.id}
+            ref={triggerRef}
+            className="space-x-2 font-mono"
+            onClick={() => router.push(`${pathname}?tabFileId=${file.id}`)}
+          >
+            <Icon className="size-3.5" />
+            <span>
+              {file.name}.{langs[file.language].extension}
+            </span>
+          </TabsTrigger>
+        </ContextMenuTrigger>
+        <ContextMenuContent>
+          <ContextMenuItem className="cursor-pointer">
+            <Pencil className="mr-2 size-4" />
+            Edit
+          </ContextMenuItem>
 
-        <ContextMenuItem
-          className="text-destructive hover:!text-destructive-11 cursor-pointer"
-          disabled={isDeletingFile}
-          onClick={async () =>
-            executeFileDelete({
-              fileId: file.id,
-              snippetId: file.snippetId,
-            })
-          }
-        >
-          {isDeletingFile ? (
-            <Loader2 className="mr-2 size-4 animate-spin" />
-          ) : (
-            <Trash className="mr-2 size-4" />
-          )}
-          Delete
-        </ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+          <DialogTrigger asChild>
+            <ContextMenuItem className="text-destructive hover:!text-destructive-11 cursor-pointer">
+              <Trash className="mr-2 size-4" />
+              Delete
+            </ContextMenuItem>
+          </DialogTrigger>
+        </ContextMenuContent>
+      </ContextMenu>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your file
+            and remove it from your snippet.
+          </DialogDescription>
+        </DialogHeader>
+
+        <div className="flex items-center gap-2">
+          <DialogClose
+            type="button"
+            className={buttonVariants({
+              size: "sm",
+              variant: "ghost",
+              className: "ml-auto min-w-24 border",
+            })}
+          >
+            Cancel
+          </DialogClose>
+
+          <Button
+            disabled={isDeletingFile}
+            className="min-w-24"
+            size="sm"
+            variant="destructive"
+            onClick={async () =>
+              executeFileDelete({
+                fileId: file.id,
+                snippetId: file.snippetId,
+              })
+            }
+          >
+            {isDeletingFile && <Loader2 className="mr-2 size-4 animate-spin" />}
+            Delete
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
