3e555dd5384c390ba0fc7479d709e2a37aa578cd
Christian Leonardo <pleasefloyd73@gmail.com>
2024-08-25 23:10:47 -0300

    feat: add new prisma union type

--- apps/web/src/@types/prisma/index.ts
+++ apps/web/src/@types/prisma/index.ts
@@ -1,10 +1,4 @@
-import type { $Enums, File, Snippet, Tag } from "@soli/db/types"
-
-export type SnippetWithFilesAndTags = Snippet & {
-  files: File[]
-  tags: Tag[]
-  collection: { slug: string } | null
-}
+import type { $Enums } from "@soli/db/types"
 
 export type FileWithSnippetInfo = {
   snippet: {
@@ -19,4 +13,33 @@ export type FileWithSnippetInfo = {
   snippetId: string
   userId: string
   updatedAt: Date
+}
+
+export type SnippetWithFilesAndTags = {
+  files: {
+    id: string
+    name: string
+    language: $Enums.Language
+    content: string
+    snippetId: string
+    userId: string
+    updatedAt: Date
+  }[]
+  tags: {
+    id: string
+    slug: string
+    userId: string
+    createdAt: Date
+    updatedAt: Date
+  }[]
+  id: string
+  title: string
+  description: string | null
+  slug: string
+  createdAt: Date
+  updatedAt: Date
+  isFavorite: boolean
+  isPublic: boolean
+  collectionId: string | null
+  userId: string
 }
