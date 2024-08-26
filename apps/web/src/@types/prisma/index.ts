0a5526d77079301eae0057854d42e2e941871c13
Christian Leonardo <pleasefloyd73@gmail.com>
2024-08-25 21:22:55 -0300

    feat: add file snippet info type

--- apps/web/src/@types/prisma/index.ts
+++ apps/web/src/@types/prisma/index.ts
@@ -1,7 +1,22 @@
-import type { File, Snippet, Tag } from "@soli/db/types"
+import type { $Enums, File, Snippet, Tag } from "@soli/db/types"
 
 export type SnippetWithFilesAndTags = Snippet & {
   files: File[]
   tags: Tag[]
   collection: { slug: string } | null
+}
+
+export type FileWithSnippetInfo = {
+  snippet: {
+    title: string
+    isPublic: boolean
+  }
+} & {
+  id: string
+  name: string
+  language: $Enums.Language
+  content: string
+  snippetId: string
+  userId: string
+  updatedAt: Date
 }
