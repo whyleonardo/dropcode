ea66dc5c0d2244289a1d95130f3426090230fe39
Christian Leonardo <pleasefloyd73@gmail.com>
2024-08-28 09:12:09 -0300

    chore: reduce opacity

--- apps/web/src/components/ui/vignette.tsx
+++ apps/web/src/components/ui/vignette.tsx
@@ -7,7 +7,7 @@ interface VignetteProps {
 export const Vignette = ({ className }: VignetteProps) => {
   return (
     <div
-      className={cn("pointer-events-none absolute opacity-35", className)}
+      className={cn("pointer-events-none absolute opacity-25", className)}
       style={{
         background:
           "radial-gradient(50% 50% at 50% 50%, var(--gray10) 0%, var(--gray1) 100%)",
