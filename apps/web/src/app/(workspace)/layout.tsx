c6e43996095b001ae15384cfb73c967012571b80
Christian Leonardo <pleasefloyd73@gmail.com>
2024-08-30 09:25:11 -0300

    refactor: improve fit analytics page on mobile screens

--- apps/web/src/app/(workspace)/layout.tsx
+++ apps/web/src/app/(workspace)/layout.tsx
@@ -1,6 +1,7 @@
 import { Breadcrumbs } from "@/components/breadcrumbs"
 import { Sidebar } from "@/components/layout/sidebar"
 import { DotPattern } from "@/components/ui/bg-dot-pattern"
+import { ScrollArea } from "@/components/ui/scroll-area"
 import { Toaster } from "@/components/ui/toaster"
 
 import { QueryProvider } from "@/providers/query-provider"
@@ -17,30 +18,32 @@ const WorkspaceLayout = ({
   return (
     <QueryProvider>
       <Toaster />
-      <div className="flex h-screen w-full overflow-hidden">
+      <div className="flex size-full overflow-hidden">
         <Sidebar />
 
-        <div className="bg-gray-1 h-full w-full overflow-hidden px-8 py-4">
+        <div className="bg-gray-1 h-full w-full overflow-hidden p-4">
           <header className="flex h-14 items-center justify-between">
             <Breadcrumbs />
           </header>
 
-          <div className="relative z-10 h-[calc(100%-3.5rem)]">
-            {children}
+          <ScrollArea className="h-[calc(100%-3.5rem)]">
+            <div className="relative z-10">
+              {children}
 
-            {modal}
-          </div>
+              {modal}
+            </div>
 
-          <DotPattern
-            width={20}
-            height={20}
-            cx={1}
-            cy={1}
-            cr={1}
-            className={cn(
-              "[mask-image:radial-gradient(800px_circle_at_center,white,transparent)]"
-            )}
-          />
+            <DotPattern
+              width={20}
+              height={20}
+              cx={1}
+              cy={1}
+              cr={1}
+              className={cn(
+                "[mask-image:radial-gradient(800px_circle_at_center,white,transparent)]"
+              )}
+            />
+          </ScrollArea>
         </div>
       </div>
     </QueryProvider>
--- apps/web/src/app/(workspace)/page.tsx
+++ apps/web/src/app/(workspace)/page.tsx
@@ -1,15 +1,12 @@
 import { LinesPerPeriodChart } from "@/components/analytics/lines-per-period-chart"
 import { MostUsedLanguages } from "@/components/analytics/most-used-languages"
-import { ScrollArea } from "@/components/ui/scroll-area"
 
 const WorkspacePage = async () => {
   return (
-    <ScrollArea className="mt-12 h-[calc(100%-3.5rem)]">
-      <div className="flex flex-wrap justify-center gap-4">
-        <MostUsedLanguages />
-        <LinesPerPeriodChart />
-      </div>
-    </ScrollArea>
+    <div className="mt-12 flex flex-wrap justify-center gap-4">
+      <MostUsedLanguages />
+      <LinesPerPeriodChart />
+    </div>
   )
 }
 
