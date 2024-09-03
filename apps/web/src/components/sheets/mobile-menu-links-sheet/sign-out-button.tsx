412bfe784885656caeaeb7223257e4ff13bd9c1e
Christian Leonardo <pleasefloyd73@gmail.com>
2024-09-03 10:59:48 -0300

    feat: add icon for sign out sheet button

--- apps/web/src/components/sheets/mobile-menu-links-sheet/sign-out-button.tsx
+++ apps/web/src/components/sheets/mobile-menu-links-sheet/sign-out-button.tsx
@@ -2,10 +2,13 @@ import { Button } from "@/components/ui/button"
 
 import { signOut } from "@/actions/auth/sign-out"
 
+import { LogOut } from "lucide-react"
+
 export const SignOutButton = () => {
   return (
     <form action={() => signOut()}>
       <Button variant="ghost" className="mt-auto w-full border">
+        <LogOut className="mr-2 size-4" />
         Sign Out
       </Button>
     </form>
